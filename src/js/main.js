84103eec0e8c4c69723dbc382e6678536581f688
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-03-29 16:46:41 +0530

    Buttons updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Button 1
+
 document.addEventListener("DOMContentLoaded", function () {
     var el = document.querySelector(".button-bird");
     var text = document.querySelector(".button-bird__text");
@@ -12,3 +14,43 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+// Button 3 JS
+
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.querySelector('.rb-button-3'),
+        loader = document.querySelector('.rb-loader'),
+        check = document.querySelector('.rb-check');
+
+    btn.addEventListener('click', function () {
+        loader.classList.add('active');
+    });
+
+    loader.addEventListener('animationend', function () {
+        check.classList.add('active');
+    });
+});
+
+
+// Button 4 JS
+
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },6000);
+  };
+  
+  var classname = document.getElementsByClassName("button4-button");
+  
+  for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', animateButton, false);
+  }
