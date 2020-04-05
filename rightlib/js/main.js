5480853941b056597ffbf2ce80c61807c6861688
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-06 01:31:39 +0530

    Hello

--- rightlib/js/main.js
+++ rightlib/js/main.js
@@ -1,4 +1,5 @@
 const gradientSelector = document.querySelector('select');
+const body = document.querySelector('body');
 const viewDivision = document.querySelector('.view-division');
 const text = document.querySelector('.inner-text');
 
@@ -6,5 +7,5 @@ gradientSelector.addEventListener('change', () => {
     let className = gradientSelector.options[gradientSelector.selectedIndex].text;
     viewDivision.className = 'rb-wrapper view-division ' + className;
     gradientSelector.className = className;
-    text.innerHTML = className;
+    body.className = className;
 });
