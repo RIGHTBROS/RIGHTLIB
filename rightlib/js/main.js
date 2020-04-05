fc401c7c145918713a2e156d78253365fb1b310a
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-06 00:56:09 +0530

    Styling Select

--- rightlib/js/main.js
+++ rightlib/js/main.js
@@ -5,5 +5,6 @@ const text = document.querySelector('.inner-text');
 gradientSelector.addEventListener('change', () => {
     let className = gradientSelector.options[gradientSelector.selectedIndex].text;
     viewDivision.className = 'rb-wrapper view-division ' + className;
+    gradientSelector.className = className;
     text.innerHTML = className;
 });
