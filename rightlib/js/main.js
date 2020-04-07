e08bb00a0eb1588238a852383f92f7df9c815cb7
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-07 13:14:33 +0530

    Adding new gradients

--- rightlib/js/main.js
+++ rightlib/js/main.js
@@ -6,6 +6,7 @@ const text = document.querySelector('.inner-text');
 gradientSelector.addEventListener('change', () => {
     let className = gradientSelector.options[gradientSelector.selectedIndex].text;
     viewDivision.className = 'rb-wrapper view-division ' + className;
+    text.innerHTML = className;
     gradientSelector.className = className;
     body.className = className;
 });
